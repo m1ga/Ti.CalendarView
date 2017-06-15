455642a03386b0d8242e7c3892fbbac66b7d6689
Michael <migamiga@web.de>
2017-06-15 21:17:56 +0200

    update

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,24 @@
 var mcv = require("/calendar-android");
 
 var calView = mcv.getView({
-	mode: "week", // week or month
+	//mode: "week", // week or month
 	arrowColor: "#AAAAAA",
 	topbarVisible: true,
-	selectionColor: "#ff6052"
+	selectionColor: "#ff6052",
+	highlightColor: "#0000FF"
 });
 
 calView.addEventListener("changed", onChangeDate);
 
 // set dots below date:
 var d = [];
-d.push(new Date())
-mcv.addEvents(d);
+d.push(new Date(2017, 5, 14).getTime())
+mcv.setEvents(d);
+
+d = [];
+d.push(new Date(2017, 5, 12).getTime())
+d.push(new Date(2017, 5, 9).getTime())
+mcv.setHighlights(d);
 
 // select today
 mcv.gotoToday();
--- app/lib/calendar-android.js
+++ app/lib/calendar-android.js
@@ -13,9 +13,14 @@ var CalendarMode = require('com.prolificinteractive.materialcalendarview.Calenda
 var DayViewDecorator = require('com.prolificinteractive.materialcalendarview.DayViewDecorator');
 var ArrayList = require('java.util.ArrayList');
 var DotSpan = require('com.prolificinteractive.materialcalendarview.spans.DotSpan');
+var ShapeDrawable = require('android.graphics.drawable.ShapeDrawable');
+var OvalShape = require('android.graphics.drawable.shapes.OvalShape');
+var InsetDrawable = require('android.graphics.drawable.InsetDrawable');
 var mcv = new MaterialCalendarView(activity);
 var dateList = [];
+var highlightDates = [];
 var onChangeDate = null;
+var highlightColor = "#ff0000";
 
 function getDay(date) {
 	date.setHours(0);
@@ -25,6 +30,14 @@ function getDay(date) {
 	return date.getTime();
 }
 
+function pixelToDp(px) {
+	return (parseInt(px) / (Titanium.Platform.displayCaps.dpi / 160));
+}
+
+function dpToPixel(dp) {
+	return (parseInt(dp) * (Titanium.Platform.displayCaps.dpi / 160));
+}
+
 var eventDecorator = new DayViewDecorator({
 	shouldDecorate: function(calDay) {
 		return (dateList.indexOf(getDay(new Date(calDay.getYear(), calDay.getMonth(), calDay.getDay()))) >= 0)
@@ -32,6 +45,19 @@ var eventDecorator = new DayViewDecorator({
 	decorate: function(view) {
 		dotSpan = new DotSpan();
 		view.addSpan(dotSpan);
+		return false;
+	}
+});
+
+var highlightDatesDecorator = new DayViewDecorator({
+	shouldDecorate: function(calDay) {
+		return (highlightDates.indexOf(getDay(new Date(calDay.getYear(), calDay.getMonth(), calDay.getDay()))) >= 0)
+	},
+	decorate: function(view) {
+		var drawable = new ShapeDrawable(new OvalShape());
+		drawable.getPaint().setColor(Color.parseColor(highlightColor));
+		insetDrawable = new InsetDrawable(drawable, 0, 0, 0, 0);
+		view.setBackgroundDrawable(insetDrawable);
 		return false;
 	}
 });
@@ -58,6 +84,7 @@ exports.getView = function(obj) {
 	}
 
 	var arrowColor = obj.arrowColor || "#fffff";
+	highlightColor = obj.highlightColor || "#ff0000";
 
 	mcv.state().edit().setCalendarDisplayMode(calMode).commit();
 	mcv.setTopbarVisible(obj.topbarVisible || false);
@@ -67,6 +94,8 @@ exports.getView = function(obj) {
 	mcv.setDateTextAppearance(Ti.App.Android.R.style.CustomDayTextAppearance);
 	mcv.setHeaderTextAppearance(Ti.App.Android.R.style.CustomDayTextAppearance);
 	mcv.setWeekDayTextAppearance(Ti.App.Android.R.style.CustomDayTextAppearance);
+
+	mcv.setTileHeight(132);
 
 	mcv.setSelectionColor(Color.parseColor(obj.selectionColor || "#000000"));
 	mcv.setSelectionMode(MaterialCalendarView.SELECTION_MODE_SINGLE);
@@ -84,12 +113,24 @@ exports.getView = function(obj) {
 	return mcv;
 }
 
-exports.addEvents = function(dates) {
+exports.setHighlights = function(dates) {
+	highlightDates = dates;
+	mcv.addDecorator(highlightDatesDecorator);
+}
+exports.getHighlights = function(dates) {
+	return highlightDates;
+}
+
+exports.setEvents = function(dates) {
 	dateList = dates;
 	mcv.addDecorator(eventDecorator);
 }
 
-mcv.addEventListener = function(str,clb){
+exports.getEvents = function() {
+	return dateList;
+}
+
+mcv.addEventListener = function(str, clb) {
 	if (str == "changed") {
 		onChangeDate = clb;
 	}
